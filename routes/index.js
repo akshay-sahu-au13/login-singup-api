43c876ce3c59b5ee5a8208facebdbe7e16138d9e
Akshay Sahu <akki5233@gmail.com>
2021-05-30 11:38:54 +0530

    Errors fixed with signup route

--- routes/index.js
+++ routes/index.js
@@ -27,23 +27,28 @@ const imageFilter = function (req, file, cb) {
 
 
 // -- Init file upload -- //
-let upload = multer({ storage: Storage, limits: {fileSize:0.5* 1024 * 1024}, fileFilter: imageFilter }).single('profileImage');
+let upload = multer({ storage: Storage, limits: { fileSize: 0.5 * 1024 * 1024 }, fileFilter: imageFilter }).single('profileImage');
 // let upload = multer({
 //     storage: Storage,
 // }).single('dp');
 
-Router.post('/signup', upload, async(req, res)=> {
+Router.post('/signup', upload, async (req, res) => {
     const data = req.body;
+
     console.log(data)
 
+
     try {
-        let user = User.findOne({email:req.body.email});
-        if (user){
-            console.log(user)
-            return res.send({message:"User already exists, please login or signup with different email"})
+        let user = await User.findOne({ email: req.body.email });
+        console.log("USER EMAIL:: ", user.email)
+        if (user.email === data.email) {
+            // console.log(user)
+            return res.send({ message: "User already exists, please login or signup with different email" })
         }
-        user = new User(data);
-        
+        user = new User({
+            ...data, profileImage: req.file.path
+        });
+
         const hashedPwd = await bcrypt.hash(data.password, 10)
         user.password = hashedPwd;
 
@@ -53,37 +58,37 @@ Router.post('/signup', upload, async(req, res)=> {
     } catch (error) {
         if (error) {
             console.log(error.message)
-            res.send({message:"Error while registering the user", error:error.message})
+            res.send({ message: "Error while registering the user", error: error.message })
         }
     }
 
 });
 
-Router.post('/login', async(req, res)=> {
+Router.post('/login', async (req, res) => {
 
     try {
-        if (req.body.email && req.body.password){
-            const user = await User.findOne({email:req.body.email});
+        if (req.body.email && req.body.password) {
+            const user = await User.findOne({ email: req.body.email });
             console.log("user:", user);
-            if (!user){
-                return res.status(404).send({message:"This email is not registered. Kindly signup first!"})
+            if (!user) {
+                return res.status(404).send({ message: "This email is not registered. Kindly signup first!" })
             }
             const pwd = await bcrypt.compare(req.body.password, user.password);
-            if (!pwd){
-                return res.status(400).send({message:"Invalid password..."})
+            if (!pwd) {
+                return res.status(400).send({ message: "Invalid password..." })
             }
 
-            const token = await jwt.sign({id:user._id, email:user.email}, "my_login_secret")
+            const token = await jwt.sign({ id: user._id, email: user.email }, "my_login_secret")
 
-            res.status(200).send({message: "Login Success", userInfo:user, token})
+            res.status(200).send({ message: "Login Success", userInfo: user, token })
 
         } else {
-            return res.status(400).send({message:"Enter both email and password!"})
+            return res.status(400).send({ message: "Enter both email and password!" })
         }
-    } 
+    }
     catch (error) {
         console.log(error.message)
-        res.send({message:"Error during Login", error:error.message});
+        res.send({ message: "Error during Login", error: error.message });
     }
 })
 
